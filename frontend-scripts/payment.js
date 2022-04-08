0c9b711190b5c3df077d1810e70781d704ac88a4
Ronan Clooney <73086339+clooneyr@users.noreply.github.com>
2022-04-09 09:56:52 +1000

    Update payment.js

--- frontend-scripts/payment.js
+++ frontend-scripts/payment.js
@@ -12,7 +12,7 @@ async function payment() {
         const algosdk = require('algosdk');
 		const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
 		const port = '';
-		const token = {'X-API-Key': 'ERJa19DtyE462CXdcoREb3mt9RQGEeM22i5CKXhz'}
+		const token = {'X-API-Key': ''}
 		
 
 		const algodClient = new algosdk.Algodv2(token, baseServer, port);
